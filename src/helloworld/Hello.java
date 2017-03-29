b3cbb9328cf8f366c8b4a35278e92bc851db9783
haliboteda <whoami_1984@qq.com>
2017-03-29 15:59:30 +0800

    Merge branch 'pr/1'
    # Conflicts:
    #	src/helloworld/Hello.java

--- src/helloworld/Hello.java
+++ src/helloworld/Hello.java
@@ -11,11 +11,11 @@ public class Hello {
 		
 		System.out.println("this is added at teda ");
 		
-<<<<<<< HEAD
 		System.out.println("this is added at gdys0033 ");
-=======
+
 		System.out.println("some one may have changed forked and this is added at the upstream teda ");
->>>>>>> 90de03af18346e278b593163f8e0b0dab5e65abe
+		System.out.println("after 1st merge changes at gdys ");
+
 	}
 
 }
